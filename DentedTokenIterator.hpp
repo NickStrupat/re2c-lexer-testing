891692e3dc1a3b0333d46c4b1c0818af46c75a65
NickStrupat <nick.strupat@gmail.com>
2020-09-03 23:10:17 -0400

    split DentedTokenIterator into header and source files

--- DentedTokenIterator.hpp
+++ DentedTokenIterator.hpp
@@ -7,58 +7,16 @@
 
 #include "lexer.hpp"
 
-template<TokenType NewlineTT, TokenType IndentationTT, TokenType IndentTT, TokenType DedentTT, TokenType EofTT>
 class DentedTokenIterator {
 	std::string_view sv;
 	uint64_t currentDentLevel;
 	bool newlineSinceLastNonTab;
 	uint64_t tabCount;
 
-	Token getDentToken(std::string_view next) {
-		if (tabCount > currentDentLevel) {
-			++currentDentLevel;
-			return { IndentTT, next };
-		}
-		else if (tabCount < currentDentLevel) {
-			--currentDentLevel;
-			return { DedentTT, next };
-		}
-	}
+	Token getDentToken(std::string_view next);
 public:
-	DentedTokenIterator(std::string_view sv) : sv(sv), currentDentLevel(), newlineSinceLastNonTab(), tabCount() {}
-
-	Token next() {
-		auto token = lex(sv);
-
-		switch (token.type) {
-			case NewlineTT:
-				newlineSinceLastNonTab = true;
-				break;
-			case IndentationTT:
-				if (newlineSinceLastNonTab)
-					++tabCount;
-				break;
-			case EofTT:
-				if (tabCount != currentDentLevel)
-					return getDentToken({ sv.data(), 0 });
-				break;
-			default:
-				if (newlineSinceLastNonTab) {
-					if (tabCount != 0) {
-						if (tabCount != currentDentLevel)
-							return getDentToken({ sv.data(), 0 });
-						else {
-							newlineSinceLastNonTab = false;
-							tabCount = 0;
-						}
-					}
-				}
-				break;
-		}
-
-		sv = sv.substr(token.sv.length());
-		return token;
-	}
+	DentedTokenIterator(std::string_view sv);
+	Token next();
 };
 
 #endif
